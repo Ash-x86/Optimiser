92b968c9f17b050d6c32b3d3f3b9385cb8da508b
Ash-x86 <119950522+Ash-x86@users.noreply.github.com>
2023-12-24 14:56:13 +0530

    Update Debloat.ps1

--- Debloat.ps1
+++ Debloat.ps1
@@ -1,11 +1,11 @@
 echo "This is a Debloater tool By hsA!"
 echo "1: CTT tool\n2: Win-Debloater"
 $choice = Read-Host -Prompt "> "
-if ($choice == 1) {
+if ($choice -eq 1) {
   irm -useb https://christitus.com/win | iex
 }
 
-elseif ($choice == 1) {
+elseif ($choice -eq 1) {
   irm -useb https://raw.githubusercontent.com/Ash-x86/Optimiser/main/Debloat/WinDebloatTools.ps1 | iex
 }
 
