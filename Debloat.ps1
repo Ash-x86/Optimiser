42db5ade7dc75c1ac8c75f4558aed4d873226a67
Ash-x86 <119950522+Ash-x86@users.noreply.github.com>
2023-12-24 14:44:44 +0530

    Create Debloat.ps1

---
+++ Debloat.ps1
@@ -0,0 +1,14 @@
+echo "This is a Debloater tool By hsA!"
+echo "1: CTT tool\n2: Win-Debloater"
+set /p $choice = "> "
+if ($choice == 1) {
+  irm -useb https://christitus.com/win | iex
+}
+
+else if ($choice == 1) {
+  irm -useb https://raw.githubusercontent.com/Ash-x86/Optimiser/main/Debloat/WinDebloatTools.ps1 | iex
+}
+
+else {
+  echo "Failed Retry command"
+}
